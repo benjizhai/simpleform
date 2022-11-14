366553c5c02711984716a596813ee775d89dab07
Akos Olasz <olaszakos@gmail.com>
2022-11-14 15:52:33 +0100

    update how-to modals with example image

--- src/form_frontend/src/lib/DistriktHowTo.svelte
+++ src/form_frontend/src/lib/DistriktHowTo.svelte
@@ -1,6 +1,10 @@
+<script>
+  import img1 from '../assets/nns.jpg';
+</script>
+
 <input type="checkbox" id="how-to-distrikt-modal" class="modal-toggle" />
 <label for="how-to-distrikt-modal" class="modal cursor-pointer">
-  <label class="modal-box relative" for="">
+  <label class="modal-box w-11/12 max-w-5xl relative" for="">
     <label
       for="how-to-distrikt-modal"
       class="btn btn-sm btn-circle absolute right-2 top-2">✕</label
@@ -8,6 +12,24 @@
     <h3 class="text-lg font-bold text-white">
       How to find your Distrikt principal
     </h3>
+    <p class="py-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse
+      veritatis rerum iure omnis similique aperiam! Praesentium sint, maxime
+      possimus, est recusandae labore amet, laudantium perspiciatis odio
+      repudiandae delectus itaque.
+    </p>
+    <p class="py-4">
+      <img src={img1} alt="" />
+    </p>
+    <p class="py-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse
+      veritatis rerum iure omnis similique aperiam! Praesentium sint, maxime
+      possimus, est recusandae labore amet, laudantium perspiciatis odio
+      repudiandae delectus itaque.
+    </p>
+    <p class="py-4">
+      <img src={img1} alt="" />
+    </p>
     <p class="py-4">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse
       veritatis rerum iure omnis similique aperiam! Praesentium sint, maxime
